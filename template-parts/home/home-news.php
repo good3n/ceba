8a11a538227a713be5c2ba047fb9d192b180aeae
Tom Gooden <good3n@gmail.com>
2019-12-26 14:48:55 -0500

    fix loop

--- template-parts/home/home-news.php
+++ template-parts/home/home-news.php
@@ -11,19 +11,20 @@
     <a href="" class="home-news__link">View All</a><!-- /.home-news__link -->
     <div class="home-news__posts">
       <?php
-        $posts = wp_get_recent_posts(array(
-          'numberposts' => 2,
-          'post_status' => 'publish'
-        ));
-        foreach($posts as $post) {
-          $excerpt = get_the_excerpt();
-          echo '<a href="'.get_permalink($post['ID']).'" class="home-news__post-link">';
-            echo '<h3 class="home-news__post-title">'.$post['post_title'].'</h3>';
-            echo '<p class="home-news__post-excerpt">'.wp_trim_words( $excerpt , '20' ).'</p>';
+        $args = array(
+         'post_status' => 'publish',
+         'posts_per_page' => 2
+        );
+        $query = new WP_Query( $args );
+        while ($query -> have_posts()) {
+          $query -> the_post();
+          echo '<a href="'.get_permalink().'" class="home-news__post-link">';
+            echo '<h3 class="home-news__post-title">'.get_the_title().'</h3>';
+            echo '<p class="home-news__post-excerpt">'.wp_trim_words( get_the_content() , '20' ).'</p>';
           echo '</a>';
-        } // endforeach;
-        wp_reset_query();
-      ?>
+        } // endwhile;
+        wp_reset_postdata();
+        ?>
     </div><!-- /.home-news__posts -->
   </div><!-- /.container -->
 </section><!-- /.home-news -->
