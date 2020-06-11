8d9ef979287d876981cc30a33834e080930bff7b
Tom Gooden <good3n@gmail.com>
2020-06-11 19:31:45 -0400

    slightly adjust intro to accomidate extra text

--- template-parts/home/home-intro.php
+++ template-parts/home/home-intro.php
@@ -11,9 +11,11 @@
     <div class="home-intro__col--one">
       <img class="home-intro__img home-intro__image--one" src="<?php echo get_template_directory_uri().'/assets/images/img1.jpg'; ?>" alt="">
       <img class="home-intro__img home-intro__image--two" src="<?php echo get_template_directory_uri().'/assets/images/img2.jpg'; ?>" alt="">
+      <p>We hope that you enjoy this special piece of property and work with our neighborhood community in keeping it safe and properly maintained.</p>
     </div><!-- /.home-intro__col--one -->
     <div class="home-intro__col--two">
-      <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Curabitur blandit tempus porttitor. Aenean lacinia bibendum nulla sed consectetur. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+      <p>Clarkston Estates Inc. is a neighborhood Beach Association located on the shores of beautiful Middle Lake in the Village of Clarkston. The land is Deeded to all property owners living within the Clarkston Estates Subdivision and is a shared lake access lot. Due paying Members will have key access to the Park and Beach.</p>
+      <p>We hope that you enjoy this special piece of property and work with our neighborhood community in keeping it safe and properly maintained.</p>
       <img class="home-intro__img home-intro__image--three" src="<?php echo get_template_directory_uri().'/assets/images/img3.jpg'; ?>" alt="">
     </div><!-- /.home-intro__col--two -->
   </div><!-- /.container -->
